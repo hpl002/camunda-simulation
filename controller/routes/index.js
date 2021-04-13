f368a8cac1c685e18726e5cced4e11e4b968e306
Herman Stoud Platou <hsplatou@gmail.com>
2021-04-13 21:21:58 +0200

    restructure

--- controller/routes/index.js
+++ controller/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/start', function(req, res, next) {
+  res.send(200)
+});
+
 module.exports = router;
