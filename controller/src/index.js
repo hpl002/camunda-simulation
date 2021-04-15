01a778abf18aee586def0122167cf0305aeb02e7
Herman Stoud Platou <hsplatou@gmail.com>
2021-04-15 22:49:49 +0200

    controller: introduce the concept of event ordering and priority
    this list if FIFO

--- controller/src/index.js
+++ controller/src/index.js
@@ -1,20 +1,52 @@
 const moment = require('moment');
 
-
-
-
 class Event {
-  constructor({priority = -1, description}) {
+  constructor({ priority = -1, description = "" }) {
     this.priority = priority;
     this.description = description;
   }
 }
 
+class PendingEvents {
+  constructor() {
+    this.pendingEvents  = {};
+  }
+
+  addEvent({ timestamp, event }) {
+    if (!this.pendingEvents[timestamp]) {
+      event.order=0;
+      this.pendingEvents[timestamp] = [event]       
+    }
+    else {
+      const events = this.pendingEvents[timestamp]
+      event.order = events.length
+
+      events.push(event)
+      // element with lowest order gets removed first
+      events.sort((a, b) => { return b.order - a.order })
+      // element with highest priority gets removed first
+      events.sort((a, b) => { return a.priority - b.priority })
+    }
+  }
+
+  getList() {
+    return this.pendingEvents
+  }
+
+  getEventsAt(timestamp) {
+    return this.pendingEvents[timestamp].events
+  }
+
+  popEvent() {
+    return this.pendingEvents[timestamp]
+  }
+}
+
 
 class Contoller {
-  constructor({startTime}) {
+  constructor({ startTime }) {
     this.clock = Date.parse(startTime);
-    this.pendingEvents = {};
+    this.pendingEvents = new PendingEvents()
   }
 
   initPendingEvents({ tokens = [] }) {
@@ -26,7 +58,7 @@ class Contoller {
       if (type.toUpperCase() === "CONSTANT") {
         const frequencyAsSeconds = moment.duration(frequency, moment.ISO_8601).asSeconds()
         for (let index = 0; index < amount; index++) {
-          this.pendingEvents[startTime] = new Event({description: "description yo"})
+          this.pendingEvents.addEvent({ timestamp: startTime, event: new Event({ description: "description yo" }) })
           startTime = startTime + frequencyAsSeconds
         }
       }
