79c4e7f3bf1fbc833b0c93fb29dbbb90e31359e8
Herman Stoud Platou <hsplatou@gmail.com>
2021-04-24 23:10:51 +0200

    lots of work on the controller

--- controller/src/index.js
+++ controller/src/index.js
@@ -1,7 +1,57 @@
 const moment = require('moment');
 const axios = require('axios');
 const { Event, PendingEvents, Worker } = require('./helper')
+const Executor = {
+  execute: async () => {
+    const arr = [1, 2, 3, 4, 5, 6, 111]
 
+    while (arr.length > 0) {
+      const s = arr.pop()
+      try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${s}`)
+        console.log(response.data.id);
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+  },
+  //const re = await axios.get(`https://jsonplaceholder.typicode.com/todos/${controller.getPendingEventsLength()}`)
+  //console.log(re.data.id);
+  execute2: async (controller) => {
+    let sdd = []
+    while (controller.getPendingEventsLength() !== 0) {
+      console.log("controller.getPendingEventsLength()", controller.getPendingEventsLength())
+      const { time, arr } = controller.popNextPendingEvent()
+      const timestamp = moment.unix(time);
+      console.info("Executing event at time", timestamp.format("HH:mm:ss"))
+      controller.setSimulationTime(time)
+      // change this to some other form of loop that pause execution
+      while (arr.length !== 0) {
+        const event = arr.pop()
+        if (event.type === "start") {
+          const response = await controller.startProcess(event, controller.processID)
+          Worker.addTaskToEvents({response, controller})
+           
+          // worker queries the process engine for new task and adds this to existing pendingEvents map 
+          
+          // add function that pulls task api and filters on:
+          //process id
+           
+          // generate completion link and add this to the new pendingEvents map
+          // update the task itslef with a variable or flag that indicates that its already been processed
+
+
+        }
+        else {
+          throw new Error("could not read event type")
+        }
+      };
+    }
+    return sdd
+  }
+}
 
 
 class Contoller {
@@ -14,7 +64,6 @@ class Contoller {
 
   initPendingEvents({ tokens = [] }) {
     let startTime = this.clock;
-
     for (const token of tokens) {
       const { frequency, type, amount } = token.distribution
       // look at what type of distribution and add elements to list accordingly
@@ -43,28 +92,6 @@ class Contoller {
     return { time: nextEventKey, arr: event }
   }
 
-  async execute() {
-    let keys = this.getPendingEvents()
-    keys = Object.keys(keys)
-    let promises
-    for (const key of keys) {
-      const { time, arr } = this.popNextPendingEvent()
-      const timestamp = moment.unix(time);
-      console.info("Executing event at time", timestamp.format("HH:mm:ss"))
-      this.setSimulationTime(time)
-      promises = arr.map(async event => {
-        if (event.type === "start") {
-          const responses = await this.startProcess(event, this.processID)
-          return responses
-        }
-        else {
-          throw new Error("could not read event type")
-        }
-      })
-    }
-    const r = await Promise.all(promises)
-    return r;
-  }
 
   setSimulationTime(pTime) {
     if (this.clock && pTime < this.clock) {
@@ -74,25 +101,30 @@ class Contoller {
     }
   };
   async startProcess(event, processID) {
-    const basePath = process.env.PROCESS_ENGINE     
+    const basePath = process.env.PROCESS_ENGINE
     const reqUrl = `${basePath}/engine-rest/process-definition/key/${processID}/start`
-    try {
-      const response = await axios.post(reqUrl, { ...event }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      return response
-    } catch (error) {
-      throw error
-    }
+    return axios.post(reqUrl, { ...event }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   };
   getPendingEvents(copy = false) {
     if (copy) return this.pendingEventsCopy
     return this.pendingEvents.getList()
   };
+
+  getPendingEventsLength(copy = false) {
+    if (copy) return this.pendingEventsCopy
+    const obj = this.pendingEvents.getList()
+    return Object.keys(obj).length
+  };
 }
 
 
 
+
+
 exports.Controller = Contoller;
+exports.Executor = Executor;
+
