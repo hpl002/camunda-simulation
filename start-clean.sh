bc5cec0d3e468dfbd0d932c7a85ff5d01e747fd8
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-06 20:33:34 +0200

    fix script for wiping h2 and elastic

--- start-clean.sh
+++ start-clean.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-rm -rf ./camunda-h2-default
+#!/bin/zsh
+rm -rf ./camunda/camunda-h2-default
 rm -rf ./camunda-optimize-3.4.0-demo/elasticsearch/elasticsearch-7.10.0/data/
-sh ./camunda/start.sh &
-sh ./camunda-optimize-3.4.0-demo/optimize-demo.sh & 
