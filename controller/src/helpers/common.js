c21a3e5b3a9a9e667ef79bda47baa8d3350277bf
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-16 19:28:15 +0200

    add func for updating random variables on process state. If never updated then perpetual loop

--- controller/src/helpers/common.js
+++ controller/src/helpers/common.js
@@ -1,25 +1,54 @@
 var moment = require('moment');
+var axios = require("axios").default;
 
 const Common = {
-    isoToSeconds: (pISO) => {
-      try {
-        const r = moment.duration(pISO, moment.ISO_8601).asSeconds()
-        if (!Number.isInteger(r)) {
-          throw new Error(`could not parse time input to seconds. Expected input in the ISO_8601 format. Received ${pISO}`)
-        }
-        else {
-          return r
-        }
-      } catch (error) {
-        console.error(error)
-        throw error
+  isoToSeconds: (pISO) => {
+    try {
+      const r = moment.duration(pISO, moment.ISO_8601).asSeconds()
+      if (!Number.isInteger(r)) {
+        throw new Error(`could not parse time input to seconds. Expected input in the ISO_8601 format. Received ${pISO}`)
       }
-    },
-    getAttribute: ({ task, attributesMap, key }) => {
-      let value = attributesMap[task.activityId] || []
-      value = value.filter(e => e.name.toUpperCase() === key)
-      return value?.[0]?.value
-    },
-  }
+      else {
+        return r
+      }
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  },
+  getAttribute: ({ task, attributesMap, key }) => {
+    let value = attributesMap[task.activityId] || []
+    value = value.filter(e => e.name.toUpperCase() === key)
+    return value?.[0]?.value
+  },
 
-  exports.Common = Common;
+  refreshRandomVariables: async ({ task }) => {
+    try {
+      let variables = await axios.get(`http://localhost:8080/engine-rest/variable-instance?processInstanceIdIn=${task.processInstanceId}`)
+
+        variables = variables.data.filter(e => e.name.toUpperCase().includes("RANDOM"))
+  
+      const obj = {
+        "modifications": {
+        }
+      }
+
+      variables.forEach(element => {
+        obj.modifications[element.name] = {
+          "value": Math.round(Math.random() * 100),
+          "type": "integer"
+        }
+      });
+    
+      let response = await axios.post(`http://localhost:8080/engine-rest/process-instance/${task.processInstanceId}/variables`,
+        obj)
+      if (response.status !== 204) throw new Error("could not update variables on process while starting task")    
+    } catch (error) {
+      console.error("could not update random variables on process")
+      throw error      
+    }
+
+  },
+}
+
+exports.Common = Common;
