3c4f6f28a36dc7de79ba6617026778e7ee994aa7
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-16 20:06:14 +0200

    patch: bug in a dependency. Hotfix for now

--- controller/src/helpers/math.js
+++ controller/src/helpers/math.js
@@ -7,15 +7,21 @@ const MathHelper = {
         type = type.toUpperCase()
         let mean = Common.getAttribute({ task, attributesMap, key: `${type}_MEAN` })
         let sd = Common.getAttribute({ task, attributesMap, key: `${type}_STANDARDDEVIATION` })
+
         
         mean = Common.isoToSeconds(mean)
         sd = Common.isoToSeconds(sd)         
         if(sd>mean){
             throw new Error("standard deviation cannot be greater than the mean. Check model", task)
         }  
+         
         const s = random.normal((mu = mean), (sigma = sd))
-        const res = Math.round(s())
-        if(res<0) throw new Error(`normal distribution returned a negative number. Must always be positive, check configuration for task ${task.activityId}`)
+
+        let res = Math.round(s())
+        while(res<0){
+            res = Math.round(s())
+        }
+
         return res
     },
 
