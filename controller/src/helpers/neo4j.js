dfc36d4af5d601d062b692ca5bab7cb6d23fcfc1
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-19 15:56:59 +0200

    some more testing to ennsure that i can use async await instead of promises

--- controller/src/helpers/neo4j.js
+++ controller/src/helpers/neo4j.js
@@ -9,20 +9,34 @@ const executeQuery = (query) => {
     return session.run(query).then(function (result) {
         logger.log("info", "pulling records from neo4j")
         let records = result.records.map(e => e.get("r"))
-        session.close();
-        driver.close();
         return records
     }).catch((err) => {
         logger.log("error", "eror while pulling records from neo4j")
-        session.close();
-        driver.close();
         console.error(err)
         throw err
+    }).finally(() => {
+        session.close();
+        driver.close();
     })
 }
 
+
+/*  
 const pQuery = "MATCH (t:Task {category: 'front-desk'})-[REQUIRES]-(s:Specialization)-[HAS]-(r:Resource) return r"
-const s = executeQuery(pQuery).then((r) => {
+const s = executeQuery2(pQuery).then((r) => {
     console.log(r)
 })
+ */
+
+
+
+
+/* (async () => {
+    const s = await executeQuery("MATCH (t:Task {category: 'front-desk'})-[REQUIRES]-(s:Specialization)-[HAS]-(r:Resource) return r")
+    console.log(s)
+})() */
+
+exports.executeQuery = executeQuery;
+
+
 
