e909664a65ee2bd5de3acfa40fc03e1d71bb6edf
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-23 20:08:21 +0200

    fix neo4j driver config: issue with rendering 64bit integer

--- controller/src/helpers/neo4j.js
+++ controller/src/helpers/neo4j.js
@@ -1,6 +1,6 @@
 var neo4j = require('neo4j-driver')
 var { logger } = require('../helpers/winston')
-var driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD));
+var driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD), { disableLosslessIntegers: true });
 var session = driver.session();
 
 
