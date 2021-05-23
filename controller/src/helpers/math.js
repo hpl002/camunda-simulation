77a5e5631c075aa48dc8942dafde425bf3ac3bd6
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-23 20:08:01 +0200

    added support for a few other distributions

--- controller/src/helpers/math.js
+++ controller/src/helpers/math.js
@@ -2,40 +2,55 @@ const { Common } = require('./common');
 const random = require('random');
 
 const MathHelper = {
-    normalDistribution({ task, attributesMap, type }) {
-        type = type.toUpperCase()
-        let mean = Common.getAttribute({ task, attributesMap, key: `${type}_MEAN` })
-        let sd = Common.getAttribute({ task, attributesMap, key: `${type}_STANDARDDEVIATION` })
-
-        
+    normalDistribution({ mean, sd }) {
+        if (typeof mean !== "string" || typeof sd !== "string") throw new Error("input has to be formatted as iso8601 duration")
         mean = Common.isoToMilliseconds(mean)
-        sd = Common.isoToMilliseconds(sd)         
-        if(sd>mean){
+        sd = Common.isoToMilliseconds(sd)
+        if (sd > mean) {
             throw new Error("standard deviation cannot be greater than the mean. Check model", task)
-        }  
-         
+        }
+
         const s = random.normal((mu = mean), (sigma = sd))
         //TODO: awaiting fix -> https://github.com/transitive-bullshit/random/issues/30
         let res = Math.round(s())
-        while(res<0){
+        while (res < 0) {
             res = Math.round(s())
         }
 
         return res
     },
 
-    random({ task, attributesMap, type }) {
-        type = type.toUpperCase()
+    bernoulli({ value }) {
+        if (typeof mean !== "value") throw new Error("input has to be formatted as iso8601 duration")
+        value = Common.isoToMilliseconds(value)
 
-        let minDuration = Common.getAttribute({ task, attributesMap, key: `${type}_MINDURATION` })
-        let maxDuration = Common.getAttribute({ task, attributesMap, key: `${type}_MAXDURATION` })
+        const s = random.bernoulli((p = value))
+        let res = Math.round(s())
+        while (res < 0) {
+            res = Math.round(s())
+        }
 
-        minDuration = Common.isoToMilliseconds(minDuration)
-        maxDuration = Common.isoToMilliseconds(maxDuration)
-
-        return random.int(minDuration, maxDuration)
+        return res
     },
 
+    poisson({ value }) {
+        if (typeof mean !== "value") throw new Error("input has to be formatted as iso8601 duration")
+        value = Common.isoToMilliseconds(value)
+
+        const s = random.poisson((lambda = value))
+        let res = Math.round(s())
+        while (res < 0) {
+            res = Math.round(s())
+        }
+
+        return res
+    },
+
+    random({ min, max }) {
+        min = Common.isoToMilliseconds(min)
+        max = Common.isoToMilliseconds(max)
+        return random.int(min, max)
+    },
 
     constant({ value }) {
         return Common.isoToMilliseconds(value)
