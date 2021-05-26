6326e1df25494a4afb268e3fff8fb65af0f26264
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-26 20:42:03 +0200

    kill session on every request

--- controller/src/helpers/neo4j.js
+++ controller/src/helpers/neo4j.js
@@ -1,28 +1,30 @@
 var neo4j = require('neo4j-driver')
 var { logger } = require('../helpers/winston')
 var driver = neo4j.driver(process.env.NEO4J_URL, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD), { disableLosslessIntegers: true });
-var session = driver.session();
 
 
 
 const executeQuery = ({query}) => {
+    var session = driver.session();
     if(!query){
         logger.log("error", "Expected a query but got nothing")
         throw new Error("Expected a query but got nothing")
     } 
     logger.log("neo4j", `running query against neo4j: ${query}`) 
-    return session.run(query).then(function (result) {
+    return session.run(query)
+    .then(function (result) {
         logger.log("neo4j", `num records returned:${result.records.length}`)
         return result.records
     }).catch((err) => {
         logger.log("error", `error on query: ${query}`)
         console.error(err)
         throw err
+    }).finally(()=>{
+        session.close();
     })
 }
 
-const close = () => {
-    session.close();
+const close = () => {     
     driver.close();
 }
 
