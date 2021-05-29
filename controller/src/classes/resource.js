2b241467f7a7240c3da95f8d020d663ba4372046
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-29 19:54:28 +0200

    add funcs to resource class

--- controller/src/classes/resource.js
+++ controller/src/classes/resource.js
@@ -1,10 +1,7 @@
-var axios = require("axios").default;
-var xml2js = require('xml2js');
-var _ = require('lodash');
-var parser = new xml2js.Parser();
 var moment = require('moment');
 var _ = require('lodash');
 const { executeQuery } = require('../helpers/neo4j')
+const { Common } = require('../helpers/common')
 
 
 class Resource {
@@ -22,8 +19,19 @@ class Resource {
     this.hasSchedule = !!hasSchedule.length > 0
   }
 
+  asEpoch({ time, day, week, year }) {
+    const hour = parseInt(time.split(":")[0])
+    const minutes = parseInt(time.split(":")[1])
+    const t = moment().year(year).isoWeek(week).isoWeekday(day).hour(hour).minute(minutes).second(0).millisecond(0).toDate().valueOf()
+    const b = Common.convertToReadableTime(t)
+    return t
+
+
+  }
+
   // basically create an entire calendar for the given resource. Starting from the current week and day     
   async buildSchedule() {
+    const year = 2021
     let weeks = await Helper.getWeeks({ id: this.id })
     for (const week of weeks) {
       let days = await Helper.getDays({ week, id: this.id })
@@ -31,7 +39,12 @@ class Resource {
         const start = await Helper.getTiming({ week, id: this.id, day, type: "Start" })
         const end = await Helper.getTiming({ week, id: this.id, day, type: "End" })
         if (start && end) {
-          _.set(this.schedule, `${week}.${day}`, { start: start, end: end })
+          _.set(this.schedule, `${week}.${day}`, {
+            start:
+              { readable: start, epoch: this.asEpoch({ time: start, day, week, year }), full: Common.convertToReadableTime(this.asEpoch({ time: start, day, week, year })) },
+            end: { readable: end, epoch: this.asEpoch({ time: end, day, week, year }), full: Common.convertToReadableTime(this.asEpoch({ time: end, day, week, year })) }
+          })
+          console.log("asd")
         }
         else {
           const message = `could not build schedule for day:${day} in week ${week} for resource ${this.id} due to missing start or end time`
@@ -40,6 +53,151 @@ class Resource {
         }
       }
     }
+  }
+
+  /**
+   *  TODO: if soonest available is today then it should return this
+   * TODO: throw error if nothing is found
+   */
+  soonestAvailability({ time }) {
+    const { week, day, full, hour } = Common.convertToReadableTime(time)
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let result = undefined
+    let weeks = Object.keys(this.schedule)
+    weeks = weeks.map(e => parseInt(e))
+    weeks = weeks.filter(e => e >= week)
+    weeks.sort(function (a, b) {
+      return a - b;
+    });
+    let numInWeek = days.findIndex(e => e === day)
+    const { start, end } = _.get(this.schedule, `${week}.${day}`)
+    if (time >= start.epoch && time <= end.epoch) {
+      result = time
+      return result
+    }
+    else {
+      weeks.forEach(week => {
+        for (let index = numInWeek; index <= days.length; index++) {
+          const { start, end } = _.get(this.schedule, `${week}.${days[index]}`)
+          if (time >= start.epoch && time <= end.epoch) {
+            result = start
+            return result
+          }
+        }
+        numInWeek = 0
+      });
+    }
+    return result
+  }
+
+  remainingTimeOfShift({ week, day, clock }) {
+    const { start, end } = _.get(this.schedule, `${week}.${day}`)
+    const remainingTime = end.epoch - clock
+    return remainingTime
+  }
+
+  findNextShift({ week, day }) {
+    // find next shift     
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    let result = {}
+
+    let weeks = Object.keys(this.schedule)
+    weeks = weeks.map(e => parseInt(e))
+    weeks = weeks.filter(e => e >= week)
+    weeks.sort(function (a, b) {
+      return a - b;
+    });
+    let dayAsNumber = days.findIndex(e => e === day) + 1
+
+    for (const week of weeks) {
+      for (let index = dayAsNumber; index <= days.length; index++) {
+        const v = _.get(this.schedule, `${week}.${days?.[index]}`)
+        if (v?.start && v?.end) {
+          result = v
+          break
+        }
+      }
+      dayAsNumber = 0
+      if (Object.keys(result).length>0) {
+        break
+      }
+    }
+
+    return result
+  }
+
+  timeFromEndOfCurrentToStartOfNext({ week, day }) {
+    const current = _.get(this.schedule, `${week}.${day}`)
+    const next = this.findNextShift({ week, day })
+    return next.start.epoch - current.end.epoch
+  }
+
+  /**
+   * @param  {} {clock
+   * @param  {} duration}    
+   */
+  addSchedulingTime({ clock, duration }) {
+    let result = undefined
+    let { week, day, full, hour } = Common.convertToReadableTime(clock)
+    let tally = 0
+
+    if (!!this.schedule?.[week]?.[day]) {
+      //has scedule for today
+      const remainingTimeShift = this.remainingTimeOfShift({ clock, week, day })
+      if (remainingTimeShift >= duration) {
+        return clock + duration
+      }
+    }
+
+    while(duration>0){
+      const { start, end } = this.findNextShift({week, day})
+      // there are no future shifts
+      if(!start && !end) return undefined
+
+      const shiftDuration = end.epoch-start.epoch
+      if(shiftDuration>duration){
+        return soonestShift.start.epoc + duration
+      }
+      else{
+        duration = duration - shiftDuration
+        tally = tally+shiftDuration
+        tally = tally+timeFromEndOfCurrentToStartOfNext({week, day})
+      }
+      ({ start, end } = findNextShift({ week, day: "Sunday" }))
+    }
+   
+
+    //if no time left today
+    // loop
+    // getSoonestAvailableShift() -> if today then return today
+    // how much time is left of shift getRemainingTimeInShift() -> if today then return remaining time of today
+    // if duration is less than remaining time then add duration to current clock and terminate
+    // else add remaining time of shift to tally 
+    // subtract remaining time from duration
+    // get time from current shift to next shift and add this to tally
+    //--iterate
+
+
+
+    if (!!this.schedule?.[week]?.[day]) {
+      const remainingTimeShift = this.remainingTimeOfShift({ clock, week, day })
+      //if remaining time in shift is less than the total duration of task then it can be completed before next shift
+      if (remainingTimeShift >= duration) {
+        result = clock + duration
+      }
+      else {
+        let remainingDuration = duration - remainingTimeShift
+        let accumulatedTime = remainingTimeShift
+        while (remainingDuration > 0) {
+          //get time from end of current shift to start of next
+
+        }
+
+
+      }
+    }
+
+    return result
   }
 
   async init() {
