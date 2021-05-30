00aceb80d9f1ccc52b269e3e9b21de953cb885ef
Herman Stoud Platou <hsplatou@gmail.com>
2021-05-30 12:17:24 +0200

    worker can now account for resources having schedules

--- controller/src/helpers/worker.js
+++ controller/src/helpers/worker.js
@@ -1,6 +1,6 @@
 var axios = require("axios").default;
-const { Event } = require('./event')
-const { Resource } = require('./resource')
+const { Event } = require('../classes/Event')
+const { Activity } = require('../classes/Activity')
 const { Common } = require('./common')
 const { logger } = require('../helpers/winston')
 const { executeQuery } = require('../helpers/neo4j')
@@ -14,27 +14,14 @@ const Worker = {
    */
   getAvailableResources: async ({ task, controller }) => {
     const response = {}
-    const query = `MATCH (a:Activity)-[]-(l:Limitations)-[boundedBy]-(s:Specialization)-[linked]-(r:Resource) WHERE a.id="${task.activityId}" return r`
-    let records = await executeQuery({ query })
-    let resources = records.map(e => e.get("r"))
-    resources = resources.map(e => e.properties.id)
-    response.potential = resources
-    resources.forEach(resource => {
-      const r = controller.resourceArr.filter(e => e.id === resource)
-      if (r.length === 0) {
-        controller.resourceArr.push(new Resource({ id: resource }))
-        logger.log("process", `could not find resource in controller. Adding new resource with id ${resource}`)
-      }
-    });
+    response.potential = task.resourceCandidates
     let available = []
 
-    //TODO: strategy for what resource to select if there are multiple available
 
-    resources.forEach(resource => {
+    response.potential.forEach(resource => {
       const arr = controller.resourceArr.filter(e => e.id === resource && e.available === true)
       if (!!arr.length > 0) available.push(arr[0])
     });
-
     response.available = available
     return response
   },
@@ -44,34 +31,10 @@ const Worker = {
    * calculates the earliest at which the specified resource is available
    */
   howLongUntilResourceAvailable: async ({ potential, controller }) => {
-    let resources = controller.resourceArr.map(r => { return { id: r.id, lockedUntil: r.lockedUntil } })
-
     let arr = controller.resourceArr.filter(e => potential.includes(e.id))
     arr = arr.map(e => e.lockedUntil)
     arr.sort((a, b) => b - a)
     return arr.pop()
-
-
-
-
-    /* let resources = await executeQuery({ query })
-    resources = resources.map(e => e.properties.name)
-
-    let earliestTime = 99999999999999999999;
-
-    resources.forEach(resource => {
-      //find earliest time at which resource is available
-      const test = controller.resourceArr.filter(e => e.id === resource)
-      if (test.length > 0) {
-        if (earliestTime > test[0].lockedUntil) earliestTime = test[0].lockedUntil
-      }
-    }); */
-
-
-    /* let arr = controller.resourceArr.filter(e => e.id === workerId)
-    arr = arr.map(e => e.lockedUntil)
-    arr.sort((a, b) => b - a); */
-    return earliestTime
   },
 
   lockResource: ({ workerId, task, controller, lockedUntil }) => {
@@ -94,61 +57,10 @@ const Worker = {
     logger.log("process", `Freeing resource: ${controller.resourceArr[index].id}`)
   },
 
-  /**
-   * @param  {} {task
-   * @param  {} timeSlotType}
-   * Calcualte a duration for the before, during, after timeslot
-   */
-  getTiming: async ({ task, timeSlotType }) => {
-    let query = `MATCH (a:Activity)-[]-(t:Timing)-[]-(d)-[]-(n:Distribution) WHERE a.id="${task.activityId}" AND "${timeSlotType}" IN labels(d) return n`
-    let record = await executeQuery({ query })
-    if (record.length > 0) {
-      record = record.map(e => e.get("n"))[0]
-
-      if (!record.properties.type) {
-        logger.log("error", "distribution is configured incorrectly. Could not find type")
-        throw new Error("distribution is configured incorrectly. Could not find type")
-      }
-      if (record.properties.type.toUpperCase() === "NORMALDISTRIBUTION") {
-        const { m, sd } = record.properties
-        return MathHelper.normalDistribution({ mean: m, sd })
-      }
-      else if (record.properties.type.toUpperCase() === "RANDOM") {
-        const { min, max } = record.properties
-        return MathHelper.random({ min, max })
-      }
-      else if (record.properties.type.toUpperCase() === "POISSON") {
-        const { value } = record.properties
-        return MathHelper.poisson({ value })
-      }
-      else if (record.properties.type.toUpperCase() === "BERNOULLI") {
-        const { value } = record.properties
-        return MathHelper.bernoulli({ value })
-      }
-      else if (record.properties.type.toUpperCase() === "CONSTANT") {
-        const { value } = record.properties
-        return MathHelper.constant({ value })
-      }
-    }
-    else return 0
-  },
-  calculateInsertionTime: async ({ task, controller, type }) => {
-    let time;
-    if (type === "start") {
-      time = await Worker.getTiming({ task, timeSlotType: "Before" })
-    }
-    else if (type === "completion") {
-      time = await Worker.getTiming({ task, timeSlotType: "During" })
-    }
-    else {
-      throw new Error("could not calcualte insertion type for this unknown type", type)
-    }
-    return parseInt(controller.clock) + time
-  },
 
   getTasks: async ({ processInstanceId }) => {
     try {
-      const { data } = await axios.get(`http://localhost:8080/engine-rest/external-task?processInstanceId=${processInstanceId}&active=true&priorityHigherThanOrEquals=0`)
+      const { data } = await axios.get(`${process.env.PROCESS_ENGINE}/engine-rest/external-task?processInstanceId=${processInstanceId}&active=true&priorityHigherThanOrEquals=0`)
       return data
     } catch (error) {
       logger.log("error", error)
@@ -177,19 +89,12 @@ const Worker = {
       }
     })
     logger.log("process", `starting process. Case: ${data.id}`)
-    await mongo.startEvent({ case_id: data.id, activity_id: "start", activity_start: Common.formatClock(controller.clock), activity_end: Common.formatClock(controller.clock), resource_id: "no-resource" })
+    //await mongo.startEvent({ case_id: data.id, activity_id: "start", activity_start: Common.formatClock(controller.clock), activity_end: Common.formatClock(controller.clock), resource_id: "no-resource" })
     return data
   },
 
   startTask: async ({ task, controller, mongo }) => {
-    let { potential, available } = await Worker.getAvailableResources({ task, controller })
-    let workerId = undefined
-    /**
-     duration of task is calculated
-     */
-    let completionTime = await Worker.calculateInsertionTime({ task, controller, type: "completion" })
-
-    const start = async (workerId,) => {
+    const start = async ({workerId, completionTime}) => {
       try {
         await Common.refreshRandomVariables({ task })
         task.workerId = workerId
@@ -197,9 +102,9 @@ const Worker = {
           "workerId": workerId,
           "lockDuration": 1800000
         }
-        response = await axios.post(`http://localhost:8080/engine-rest/external-task/${task.id}/lock`, body)
+        response = await axios.post(`${process.env.PROCESS_ENGINE}/engine-rest/external-task/${task.id}/lock`, body)
         if (response.status !== 204) throw new Error("could not lock task")
-        logger.log("process", `Starting task ${task.activityId}at ${ Common.formatClock(controller.clock)} with resoruce ${task.workerId }}`)
+        logger.log("process", `Starting task ${task.activityId}at ${Common.formatClock(controller.clock)} with resoruce ${task.workerId}}`)
         await mongo.startTask({ case_id: task.processInstanceId, activity_id: task.activityId, activity_start: Common.formatClock(controller.clock), resource_id: task.workerId })
         return { task, startTime: completionTime, type: "complete task" }
       } catch (error) {
@@ -208,31 +113,50 @@ const Worker = {
       }
     }
 
+    let { potential, available } = await Worker.getAvailableResources({ task, controller })
+    // available here meaning that the resource is not tied to another task, it does not accout for scheduling
+    let workerId = undefined
+    let completionTime = 0
 
-    if (potential.length === 0) {
+
+
+    if (!task.hasResourceCandidates) {
+      // no resource no schedule
       workerId = "no-resource"
-      const s = await start(workerId)
+      completionTime = clock+duration
+      const s = await start({workerId, completionTime})
       return s
     }
     else {
-
       if (available.length > 0) {
-        //TODO:  could use a strategy to determine what resource to select if there are multipe available. Spread workload evenly?     
-        //TODO:  check if there is enough time left to even complete the task
-        workerId = available[0].id
+        const resourcesWithSchedules = available.filter(r => r.hasSchedule === true)
+        const resourcesWithoutSchedules = available.filter(r => r.hasSchedule === false)
+        if (!!resourcesWithSchedules.length === !!resourcesWithoutSchedules.length) throw new Error("Task cannot have resources with and without schedules. These are mutually exclusive")         
+
+        if (!!resourcesWithSchedules.length) {
+          /* pass in clock and duration an then get back a time at which the task will be finished that accounts for duration  */
+          available.map(r => r.earliestAvailable = r.addSchedulingTime({ clock: controller.clock, duration: task.timing.duration()}))
+          avalable = available.filter(e=>e.earliestAvailable!==undefined)
+          if(available.length===0) throw new Error("Task duration exceeds the scheduling of any potential resources. There are no resources which can complete this task with their given schedule")
+          available.sort((a, b) => b.earliestAvailable - a.earliestAvailable)
+          console.log(`calculated insertion times for ${available.length} resources`)
+          
+          workerId = available[0].id
+          completionTime = available[0].earliestAvailable
+        }
+
         Worker.lockResource({ workerId, task, controller, lockedUntil: completionTime })
-        const s = await start(workerId)
+        const s = await start({workerId, completionTime})
         return s
       }
       else {
-        //TODO:  check if there is enough time left to even complete the task
-        completionTime = await Worker.howLongUntilResourceAvailable({ potential, controller })
+        // here we account for scenario where resource is bussy with other task         
+        const startTime = await Worker.howLongUntilResourceAvailable({ potential, controller })
         logger.log("process", `Found resource on task and resources is not available. Rescheduling to ${Common.formatClock(completionTime)}`)
-        return { task, startTime: completionTime, type: "start task", reason: "Reschedule: Could not find any available resource" }
+        return { task, startTime, type: "start task", reason: "Reschedule: Could not find any available resource" }
       }
     }
   },
-
   completeTask: async ({ task, controller, mongo }) => {
     if (task.workerId && task.workerId !== "no-resource") {
       Worker.freeResource({ task, controller })
@@ -242,9 +166,9 @@ const Worker = {
         "workerId": task.workerId,
         "variables": {}
       }
-      response = await axios.post(`http://localhost:8080/engine-rest/external-task/${task.id}/complete`, body)
+      response = await axios.post(`${process.env.PROCESS_ENGINE}/engine-rest/external-task/${task.id}/complete`, body)
       if (response.status !== 204) throw new Error("could not complete task")
-      logger.log("process", `Completing task ${task.activityId}at ${ Common.formatClock(controller.clock)} with resoruce ${task.workerId }}`)
+      logger.log("process", `Completing task ${task.activityId}at ${Common.formatClock(controller.clock)} with resoruce ${task.workerId}}`)
       await mongo.completeTask({ case_id: task.processInstanceId, activity_id: task.activityId, activity_end: Common.formatClock(controller.clock) })
     } catch (error) {
       logger.log("error", error)
@@ -258,9 +182,14 @@ const Worker = {
     if (tasks.length !== 0) logger.log("info", `fetching new tasks from Camunda. Found a total of ${tasks.length} new tasks`)
     while (tasks.length !== 0) {
       const currTask = tasks.pop()
-      const timeStamp = await Worker.calculateInsertionTime({ task: currTask, controller, type: "start" })
+      if (!controller.taskMap[currTask.activityId]) {
+        const newTask = new Activity({ activityId: currTask.activityId })
+        await newTask.init()
+        controller.taskMap[currTask.activityId] = newTask
+      }
       //TODO: How should tasks be prioritized? Should new fetched evens be configured to run as soon as possible       
-      controller.addEvent({ startTime: timeStamp, event: new Event({ task: { ...currTask }, type: "start task" }) })
+      let startTime = controller.taskMap[currTask.activityId].timing.before() + controller.clock
+      controller.addEvent({ startTime, event: new Event({ task: { ...currTask, ...controller.taskMap[currTask.activityId] }, type: "start task" }) })
       await Worker.setPriority({ processInstanceId: currTask.id })
     }
   },
@@ -272,7 +201,7 @@ const Worker = {
    */
   setPriority: async ({ processInstanceId }) => {
     try {
-      const response = await axios.put(`http://localhost:8080/engine-rest/external-task/${processInstanceId}/priority`, {
+      const response = await axios.put(`${process.env.PROCESS_ENGINE}/engine-rest/external-task/${processInstanceId}/priority`, {
         "priority": -1
       }, {
         headers: {
