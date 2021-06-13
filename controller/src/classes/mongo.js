64b750e52ca6c671802b29f068390c0fe56390fb
Herman Stoud Platou <hsplatou@gmail.com>
2021-06-14 00:18:09 +0200

    add forced timout due to mongoose being slow as heck

--- controller/src/classes/mongo.js
+++ controller/src/classes/mongo.js
@@ -1,18 +1,19 @@
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var { logger } = require('../helpers/winston');
 const sleep = require('util').promisify(setTimeout)
+var async = require("async");
 
 
 
 class Mongo {
-  constructor({ collection = "logs", db="simulation" }) {
+  constructor({ collection = "logs", db = "simulation" }) {
     this.collection = collection;
-    this.connectionString = `${process.env.MONGO}/${db}` 
-    
+    this.connectionString = `${process.env.MONGO}/${db}`
+
     this.init()
     var schema = ""
-    if(collection ==="logs"){
+    if (collection === "logs") {
       schema = new Schema({
         case_id: {
           type: String,
@@ -38,13 +39,13 @@ class Mongo {
         },
       }, { timestamps: { createdAt: 'created_at' }, })
     }
-    else{
-      schema = new Schema({    
+    else {
+      schema = new Schema({
         id: {
           type: String,
           required: true,
           immutable: true
-        },              
+        },
         camunda: {
           type: String,
           required: true,
@@ -54,41 +55,42 @@ class Mongo {
           type: String,
           required: false,
           immutable: true
-        },         
+        },
       }, { timestamps: { createdAt: 'created_at' }, })
     }
     this.schema = schema
     this.model = mongoose.model("configs", this.schema);
   }
-  
+
   init() {
     mongoose.connect(this.connectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 21474899,
-    });     
+    });
   }
-  
+
   async getConfig({id}) {          
     logger.log("mongo", `Retrieving configs`)     
-    const temp = await this.model.find({id:id})       
-    return temp     
+    await sleep(500)
+    const temp = await this.model.find({id:id})   
+    return temp         
   }
-  
-   async addConfig({id,camunda, neo4j}) {          
-    logger.log("mongo", `Uploading configs with id:${id}`)     
-    await this.model.create({ camunda, neo4j, id}, function (err, small) {
+
+  async addConfig({ id, camunda, neo4j }) {
+    logger.log("mongo", `Uploading configs with id:${id}`)
+    await this.model.create({ camunda, neo4j, id }, function (err, small) {
       if (err) throw err
     });
   }
 
-  async startEvent({ case_id, activity_id, activity_start, activity_end, resource_id }) {          
+  async startEvent({ case_id, activity_id, activity_start, activity_end, resource_id }) {
     logger.log("mongo", `Mongo Logging:Starting process case_id:${case_id}`)
     logger.log("mongo", `Mongo Logging:Starting process activity_id:${activity_id}`)
     logger.log("mongo", `Mongo Logging:Starting process activity_start:${activity_start}`)
     logger.log("mongo", `Mongo Logging:Starting process activity_end:${activity_end}`)
-    logger.log("mongo", `Mongo Logging:Starting process resource_id:${resource_id}`)     
-     
+    logger.log("mongo", `Mongo Logging:Starting process resource_id:${resource_id}`)
+
     await this.model.create({ case_id, activity_id, activity_start, activity_end, resource_id }, function (err, small) {
       if (err) throw err
     });
@@ -98,8 +100,8 @@ class Mongo {
     logger.log("mongo", `Mongo Logging:Starting task case_id:${case_id}`)
     logger.log("mongo", `Mongo Logging:Starting task activity_id:${activity_id}`)
     logger.log("mongo", `Mongo Logging:Starting task activity_start:${activity_start}`)
-    logger.log("mongo", `Mongo Logging:Starting task resource_id:${resource_id}`)     
-    if (!(case_id && activity_id &&activity_start && resource_id)) throw new Error("failed while trying to start task due to invalid params")
+    logger.log("mongo", `Mongo Logging:Starting task resource_id:${resource_id}`)
+    if (!(case_id && activity_id && activity_start && resource_id)) throw new Error("failed while trying to start task due to invalid params")
     await this.model.create({ case_id, activity_id, activity_start, resource_id }, function (err, small) {
       if (err) throw err
     });
@@ -112,7 +114,7 @@ class Mongo {
     if (!activity_end) throw new Error("cannot complete task with no end time")
 
     let filter = { "case_id": case_id, "activity_id": activity_id };
-     
+
     let foundDocument = ""
     let counter = 0
     const getDocument = async () => {
@@ -123,9 +125,9 @@ class Mongo {
         }
       }).sort({ "created_at": "-1" }).limit(1).lean()
       counter = counter + 1
-      if (temp.length !== 1) {         
-          await sleep(500)
-          await getDocument()
+      if (temp.length !== 1) {
+        await sleep(500)
+        await getDocument()
       }
       else {
         counter = 0
@@ -133,16 +135,16 @@ class Mongo {
       }
     }
     let v = 0;
-    const updateModel = async () => {       
+    const updateModel = async () => {
       logger.log("mongo", "trying to update document in mongo")
       const res = await this.model.updateOne({
         _id: foundDocument._id
       }, { activity_end })
-      if (!!res.n!==true) {
-        v = v+1
-        await sleep(500)         
+      if (!!res.n !== true) {
+        v = v + 1
+        await sleep(500)
         logger.log("mongo", "trying to update document in mongo AGAIN")
-        await updateModel()         
+        await updateModel()
       }
     }
     if (counter <= 3) {
