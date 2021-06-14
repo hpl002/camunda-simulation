ed6c4657f6ed28c78459052cf63077d3a8acbd3a
Herman Stoud Platou <hsplatou@gmail.com>
2021-06-14 09:40:42 +0200

    add config helper

---
+++ controller/test/helpers.js
@@ -0,0 +1,9 @@
+class Config {
+    constructor() {
+        this.log = { id: "", data: [] }
+        this.files = { neo4j: "", bpmn: "", id:"" }
+        this.id = ""
+    }
+}
+
+exports.Config = Config;
