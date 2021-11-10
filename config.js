44f2c23e9766bb1ed6cfadff0efa4b4b28dbc9b6
Herman Stoud Platou <hsplatou@gmail.com>
2021-11-10 21:28:59 +0100

    delested unused config exports

--- config.js
+++ config.js
@@ -2,10 +2,5 @@ module.exports = {
     processEngine: `${process.env.PROCESS_ENGINE_HOST}:${process.env.PROCESS_ENGINE_PORT}`,
     mongo: `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
     mongoHTTP: `http://localhost:${process.env.MONGO_PORT}`,     
-    neo4j: `${process.env.NEO4J_HOST}:${process.env.NEO4J_PORT}`,
-    neo4j_username: `${process.env.NEO4J_USERNAME}`,
-    neo4j_password: `${process.env.NEO4J_PASSWORD}`,
-    neo4jHTTP: `http://localhost:${process.env.NEO4J_PORT}`,     
-    neo4j: `${process.env.NEO4J_HOST}:${process.env.NEO4J_PORT}`,
     controller: `http://localhost:${process.env.CONTROLLER_PORT}`
 }
