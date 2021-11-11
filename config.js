4cc856d43deac9c3dae9c3186af1f789dcac757e
Herman Stoud Platou <hsplatou@gmail.com>
2021-11-12 00:08:56 +0100

    del unused config exports

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    processEngine: `${process.env.PROCESS_ENGINE_HOST}:${process.env.PROCESS_ENGINE_PORT}`,
-    mongo: `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
-    mongoHTTP: `http://localhost:${process.env.MONGO_PORT}`,     
+    processEngine: `${process.env.PROCESS_ENGINE_HOST}:${process.env.PROCESS_ENGINE_PORT}`,     
     controller: `http://localhost:${process.env.CONTROLLER_PORT}`
 }
