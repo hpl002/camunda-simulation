51f3172ddbcf49127c45fcb0388d089ff3d29b44
Herman Stoud Platou <hsplatou@gmail.com>
2021-11-28 14:44:38 +0100

    mv test to core

--- test/core-functionality/core.test.js
+++ test/core-functionality/core.test.js
@@ -34,12 +34,12 @@ describe('core functionality', () => {
         })
 
         describe('negative', () => {
-            test('upload invalid config: incorrect token description', async () => {                 
+            test('upload invalid config: incorrect token description', async () => {
                 let payload = require(`${process.env.PWD}/test/core-functionality/data/payload.json`)
                 payload = JSON.parse(JSON.stringify(payload))
                 delete payload.tokens[0].variables[0].type
                 try {
-                    await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload})                     
+                    await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload })
                 } catch (error) {
                     const { status, data } = error.response
                     expect(status === 400).toBe(true);
@@ -49,7 +49,7 @@ describe('core functionality', () => {
             test('upload invalid config: missing file', async () => {
                 let payload = require(`${process.env.PWD}/test/core-functionality/data/payload.json`)
                 try {
-                    await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload, options: { nofile: true } })                     
+                    await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload, options: { nofile: true } })
                 } catch (error) {
                     const { status, data } = error.response
                     expect(status === 400).toBe(true);
@@ -72,11 +72,11 @@ describe('core functionality', () => {
 
     });
 
-    describe('execute model', () => {         
+    describe('execute model', () => {
 
         describe('positive', () => {
             test('upload config and exexute simulation: get response as csv', async () => {
-                const { status } = await upload({ modelPath:  `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/payload.json`) })
+                const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/payload.json`) })
                 expect(status === 201).toBe(true);
 
                 let config = {
@@ -95,7 +95,7 @@ describe('core functionality', () => {
             });
 
             test('upload config and exexute simulation: get response as json', async () => {
-                const { status } = await upload({ modelPath:  `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/payload.json`) })
+                const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/payload.json`) })
                 expect(status === 201).toBe(true);
 
                 let config = {
@@ -118,7 +118,7 @@ describe('core functionality', () => {
 
         describe('negative', () => {
             test('upload config and exexute simulation: invalid response type', async () => {
-                const { status } = await upload({ modelPath:  `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/payload.json`) })
+                const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/test.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/payload.json`) })
                 expect(status === 201).toBe(true);
 
                 let config = {
@@ -144,7 +144,7 @@ describe('core functionality', () => {
     });
 
     describe('check produced log', () => {
-        test('check log of very simple execution with 2 tokens', async () => {             
+        test('check log of very simple execution with 2 tokens', async () => {
             const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/task/single-task.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/task/2-tokens-payload.json`) })
             expect(status === 201).toBe(true);
 
@@ -157,15 +157,15 @@ describe('core functionality', () => {
 
             const response = await axios(config)
             expect(response.status === 200).toBe(true);
-            expect( Object.keys(response.data).length === 2).toBe(true);
+            expect(Object.keys(response.data).length === 2).toBe(true);
             Object.keys(response.data).forEach(key => {
-                const activities = response.data[key].map(e=>e.activity_id)
-                expect( activities.length === 3).toBe(true);
-                expect( activities.filter(e=>["start", "end"].includes(e)).length === 2).toBe(true);
+                const activities = response.data[key].map(e => e.activity_id)
+                expect(activities.length === 3).toBe(true);
+                expect(activities.filter(e => ["start", "end"].includes(e)).length === 2).toBe(true);
             });
-        });   
-        
-        test('check log of very simple execution with 20 tokens', async () => {             
+        });
+
+        test('check log of very simple execution with 20 tokens', async () => {
             const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/task/single-task.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/task/20-tokens-payload.json`) })
             expect(status === 201).toBe(true);
 
@@ -178,15 +178,15 @@ describe('core functionality', () => {
 
             const response = await axios(config)
             expect(response.status === 200).toBe(true);
-            expect( Object.keys(response.data).length === 20).toBe(true);
+            expect(Object.keys(response.data).length === 20).toBe(true);
             Object.keys(response.data).forEach(key => {
-                const activities = response.data[key].map(e=>e.activity_id)
-                expect( activities.length === 3).toBe(true);
-                expect( activities.filter(e=>["start", "end"].includes(e)).length === 2).toBe(true);
+                const activities = response.data[key].map(e => e.activity_id)
+                expect(activities.length === 3).toBe(true);
+                expect(activities.filter(e => ["start", "end"].includes(e)).length === 2).toBe(true);
             });
-        });  
+        });
 
-        test('check log of very simple execution with 200 tokens', async () => {             
+        test('check log of very simple execution with 200 tokens', async () => {
             const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/task/single-task.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/task/200-tokens-payload.json`) })
             expect(status === 201).toBe(true);
 
@@ -199,13 +199,39 @@ describe('core functionality', () => {
 
             const response = await axios(config)
             expect(response.status === 200).toBe(true);
-            expect( Object.keys(response.data).length === 200).toBe(true);
+            expect(Object.keys(response.data).length === 200).toBe(true);
             Object.keys(response.data).forEach(key => {
-                const activities = response.data[key].map(e=>e.activity_id)
-                expect( activities.length === 3).toBe(true);
-                expect( activities.filter(e=>["start", "end"].includes(e)).length === 2).toBe(true);
+                const activities = response.data[key].map(e => e.activity_id)
+                expect(activities.length === 3).toBe(true);
+                expect(activities.filter(e => ["start", "end"].includes(e)).length === 2).toBe(true);
             });
-        });  
+        });
+
+        test('log entry contains reference to its originating token', async () => {
+            const { status } = await upload({ modelPath: `${process.env.PWD}/test/core-functionality/data/task/single-task.bpmn`, payload: require(`${process.env.PWD}/test/core-functionality/data/task/2-tokens-payload.json`) })
+            expect(status === 201).toBe(true);
+
+            let config = {
+                method: 'post',
+                url: `${appconfigs.controller}/start`,
+                headers: {},
+                data: { "response": "json" }
+            };
+
+            const response = await axios(config)
+            expect(response.status === 200).toBe(true);
+            expect(Object.keys(response.data).length === 2).toBe(true);
+
+            const events = []
+
+            Object.keys(response.data).forEach(id => {
+                events.push(...response.data[id])
+            });
+
+            events.forEach(event => {
+                expect(!!event.token_id).toBe(true);
+            });
+        });
     });
 
 })
--- test/gateways/simple.test.js
+++ test/gateways/simple.test.js
@@ -42,31 +42,7 @@ describe('Test gateways and variables', () => {
         expect(!!process2.find(e => e.activity_id === "age-gt-20")).toBe(true);
     });
 
-    test('Process execution contains reference to its originating token', async () => {
-        const { status } = await upload({ modelPath: `${process.env.PWD}/test/gateways/data/exclusive.bpmn`, payload: require(`${process.env.PWD}/test/gateways/data/exclusive.json`) })
-        expect(status === 201).toBe(true);
-
-        let config = {
-            method: 'post',
-            url: `${appconfigs.controller}/start`,
-            headers: {},
-            data: { "response": "json" }
-        };
-
-        const response = await axios(config)
-        expect(response.status === 200).toBe(true);
-        expect(Object.keys(response.data).length === 2).toBe(true);
-
-        const events = []
-
-        Object.keys(response.data).forEach(id => {
-            events.push(...response.data[id])
-        });
-
-        events.forEach(event => {
-            expect(!!event.token_id).toBe(true);
-        });
-    });
+     
 
     test('Process with parallel gateway', async () => {
         const { status } = await upload({ modelPath: `${process.env.PWD}/test/gateways/data/parallel.bpmn`, payload: require(`${process.env.PWD}/test/gateways/data/parallel.json`) })
